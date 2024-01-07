178eedd8dad006360e01d69898388c9068925816
swpiotr5 <swpiotr5@interia.pl>
2024-01-07 19:38:05 +0100

    added iterator and factory pattern

--- Reader.py
+++ Reader.py
@@ -2,7 +2,6 @@ import cv2
 import os
 import numpy as np
 import random
-import numpy as np
 from scipy.stats import norm
 
 class BlobDetector:
@@ -40,39 +39,59 @@ class BlobEntity:
         last_known_position = self.track[-1]
         return ((blob_center[0] - last_known_position[0]) ** 2 + (blob_center[1] - last_known_position[1]) ** 2) ** 0.5
 
+class BlobEntityFactory:
+    @staticmethod
+    def create_blob_entity(initial_blob, color):
+        return BlobEntity(initial_blob, color)
+
+class BlobEntityIterator:
+    def __init__(self, entities):
+        self.entities = entities
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.entities):
+            entity = self.entities[self.index]
+            self.index += 1
+            return entity
+        else:
+            raise StopIteration
+
 class BlobEntityTracker:
     def __init__(self, blobs, colors):
-        self.entities = [BlobEntity(blob, color) for blob, color in zip(blobs[0], colors)]
+        self.entities = [BlobEntityFactory.create_blob_entity(blob, color) for blob, color in zip(blobs[0], colors)]
         self.blobs = blobs[1:]
         self.colors = colors
         self.SOME_THRESHOLD = 500
 
     def track_entities(self):
         for blobs in self.blobs:
-            # Create a list of entities that are not yet assigned to a blob
             unassigned_entities = self.entities.copy()
 
             blobs = list(blobs)
             blobs.sort(key=lambda blob: min(entity.distance_to(blob) for entity in unassigned_entities))
 
             for blob in blobs:
-                # If there are no unassigned entities left, create a new one
                 if not unassigned_entities:
-                    self.entities.append(BlobEntity(blob, random.choice(self.colors)))
+                    self.entities.append(BlobEntityFactory.create_blob_entity(blob, random.choice(self.colors)))
                     continue
 
-                # Find the closest entity that is not yet assigned to a blob
                 closest_entity = min(unassigned_entities, key=lambda entity: entity.distance_to(blob))
 
                 if closest_entity.distance_to(blob) < self.SOME_THRESHOLD:
                     closest_entity.add_blob(blob)
                     unassigned_entities.remove(closest_entity)
                 else:
-                    self.entities.append(BlobEntity(blob, random.choice(self.colors)))
+                    self.entities.append(BlobEntityFactory.create_blob_entity(blob, random.choice(self.colors)))
 
-            # Remove entities that were not assigned to any blob
             for entity in unassigned_entities:
                 self.entities.remove(entity)
+
+    def get_entities_iterator(self):
+        return BlobEntityIterator(self.entities)
 
 class BlobAnimation:
     def __init__(self, tracker=None, input_dir='pictures', delay=1000):
@@ -109,7 +128,9 @@ class BlobAnimation:
                     self.tracker = BlobEntityTracker(self.detector.get_blobs()[:i+1], self.entity_colors)
                     self.tracker.track_entities()
 
-                    for entity in self.tracker.entities:
+                    entities_iterator = self.tracker.get_entities_iterator()
+
+                    for entity in entities_iterator:
                         track = entity.get_track()
                         entity_color = entity.color
                         for j in range(1, len(track)):
@@ -122,6 +143,28 @@ class BlobAnimation:
 
         cv2.destroyAllWindows()
 
+class AssociationManager:
+    @staticmethod
+    def associate_blobs(previous_probabilities, current_probabilities):
+        associations = []
+
+        for j in range(len(current_probabilities)):
+            max_probability = -1
+            best_match = -1
+
+            for k in range(len(previous_probabilities)):
+                probability_ij = current_probabilities[j]
+                prev_prob = previous_probabilities[k]
+
+                prob = prev_prob * probability_ij
+                if prob > max_probability:
+                    max_probability = prob
+                    best_match = k
+
+            associations.append((j, best_match))
+
+        return associations
+
 class JPDA:
     def __init__(self, blobs):
         self.blobs = blobs
@@ -131,23 +174,18 @@ class JPDA:
     def calculate_probabilities(self):
         for i in range(1, len(self.blobs)):
             current_blobs = self.blobs[i]
-            previous_blobs = self.blobs[i-1]
+            previous_blobs = self.blobs[i - 1]
             probabilities_for_current_blobs = []
 
             for blob in current_blobs:
                 distances = [np.linalg.norm(np.mean(blob, axis=0) - np.mean(previous_blob, axis=0)) for previous_blob in previous_blobs]
-                # Obliczanie średniej i odchylenia standardowego odległości
                 mean_distance = np.mean(distances)
                 std_distance = np.std(distances)
 
-                # Obliczanie prawdopodobieństwa na podstawie rozkładu normalnego
                 probabilities = [norm.pdf(distance, loc=mean_distance, scale=std_distance) for distance in distances]
-                
-                # Użycie zasady 3 sigm: bloby w odległości większej niż 3 sigma od średniej są odrzucane
                 threshold = mean_distance + 3 * std_distance
                 probabilities = [prob if dist < threshold else 0.0 for dist, prob in zip(distances, probabilities)]
 
-                # Normalizacja prawdopodobieństw
                 total = sum(probabilities)
                 probabilities = [probability / total for probability in probabilities]
 
@@ -156,27 +194,44 @@ class JPDA:
             self.probabilities.append(probabilities_for_current_blobs)
 
     def get_tracks(self):
+        association_manager = AssociationManager()
+
         for i in range(len(self.probabilities)):
-            tracks_for_current_blobs = [np.argmax(probabilities) for probabilities in self.probabilities[i]]
+            tracks_for_current_blobs = []
+            previous_probabilities = []
+            if i > 0:
+                previous_probabilities = self.probabilities[i - 1]
+
+            for j in range(len(self.probabilities[i])):
+                try:
+                    associations = association_manager.associate_blobs(previous_probabilities, self.probabilities[i][j])
+                    tracks_for_current_blobs.extend(associations)
+                except TypeError:
+                    pass
+
             self.tracks.append(tracks_for_current_blobs)
 
         return self.tracks
 
-animation = BlobAnimation()
+def main():
+    animation = BlobAnimation()
 
-animation.detector.detect_blobs()
-blobs = animation.detector.get_blobs()
+    animation.detector.detect_blobs()
+    blobs = animation.detector.get_blobs()
 
-if blobs:
-    colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in range(len(blobs))]
-    tracker = BlobEntityTracker(blobs, colors)
-    tracker.track_entities()
+    if blobs:
+        colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in range(len(blobs))]
+        tracker = BlobEntityTracker(blobs, colors)
+        tracker.track_entities()
 
-    animation = BlobAnimation(tracker)
-    animation.run()
+        animation = BlobAnimation(tracker)
+        animation.run()
 
-    jpda = JPDA(blobs)
-    jpda.calculate_probabilities()
-    tracks = jpda.get_tracks()
-else:
-    print("No blobs detected.")
+        jpda = JPDA(blobs)
+        jpda.calculate_probabilities()
+        tracks = jpda.get_tracks()
+    else:
+        print("No blobs detected.")
+
+if __name__ == "__main__":
+    main()
